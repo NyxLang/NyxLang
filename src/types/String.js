853ac61e3e78583aff6383f1115e32def5953eae
Jason Barr <jason@jasonsbarr.com>
2021-03-22 22:04:59 -0500

    Refactor String to use ES6 iterator

--- src/types/String.js
+++ src/types/String.js
@@ -7,6 +7,21 @@ class NyxString extends NyxPrimitive {
     this.__data__ = v.graphemes(value);
     this.length = this.__data__.length;
   }
+
+  [Symbol.iterator]() {
+    this.current = 0;
+    return this;
+  }
+
+  next() {
+    if (this.current < this.length) {
+      return {
+        value: new NyxString(this.__data__[this.current++]),
+        done: false,
+      };
+    }
+    return { done: true };
+  }
 }
 
 module.exports = NyxString;
