11c0370878de012cbef8c73e62219f265247e404
Jason Barr <jason@jasonsbarr.com>
2021-03-22 20:57:18 -0500

    Make sure value of string is passed to grapheme splitter

--- src/types/String.js
+++ src/types/String.js
@@ -4,7 +4,8 @@ const NyxPrimitive = require("./Primitive");
 class NyxString extends NyxPrimitive {
   constructor(value) {
     super(value, "String", "string");
-    this.__data__ = v.graphemes();
+    this.__data__ = v.graphemes(value);
+    this.length = this.__data__.length;
   }
 }
 
