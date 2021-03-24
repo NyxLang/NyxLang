5972c99f950d14bec5e0998a2c89b17c64607405
Jason Barr <jason@jasonsbarr.com>
2021-03-24 13:44:44 -0500

    Add a few string methods

--- src/types/String.js
+++ src/types/String.js
@@ -50,12 +50,58 @@ class NyxString extends NyxPrimitive {
     return { done: true };
   }
 
-  upcase() {
-    return new NyxString(v.upperCase(this.__value__));
+  "camel-case"() {
+    return new NyxString(v.camelCase(this.__value__));
+  }
+
+  capitalize() {
+    return new NyxString(v.capitalize(this.__value__));
+  }
+
+  decapitalize() {
+    return new NyxString(v.decapitalize(this.__value__));
   }
 
   downcase() {
     return new NyxString(v.lowerCase(this.__value__));
+  }
+
+  first(length) {
+    let l = length.__value__.toString();
+    let chars = this.__data__.slice(0, l);
+    return new NyxString(chars.join(""));
+  }
+
+  hyphenate() {
+    return new NyxString(v.kebabCase(this.__value__));
+  }
+
+  last(length) {
+    let len = parseInt(length.__value__.toString());
+    let start = this.__data__.length - len;
+    let chars = this.__data__.slice(start);
+    return new NyxString(chars.join(""));
+  }
+
+  "snake-case"() {
+    return new NyxString(v.snakeCase(this.__value__));
+  }
+
+  "swap-case"() {
+    return new NyxString(v.swapCase(this.__value__));
+  }
+
+  "title-case"() {
+    return new NyxString(v.titleCase(this.__value__));
+  }
+
+  truncate(length, end = "...") {
+    l = parseInt(length.__value__.toString());
+    return new NyxString(v.prune(this.__value__, l, end.toString()));
+  }
+
+  upcase() {
+    return new NyxString(v.upperCase(this.__value__));
   }
 }
 
