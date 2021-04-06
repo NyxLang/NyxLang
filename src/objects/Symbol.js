863046532fad79a88ad446952d46ec44865b606e
Jason Barr <jason@jasonsbarr.com>
2021-04-06 12:21:26 -0500

    Hash symbol name

--- src/objects/Symbol.js
+++ src/objects/Symbol.js
@@ -1,9 +1,8 @@
-const uuid = require("uuid");
 const hash = require("object-hash");
 
 class NyxSymbol {
   constructor(name) {
-    this.__object_id__ = hash(uuid.v4());
+    this.__object_id__ = hash(name);
     this.__class__ = S.Symbol;
     this.__type__ = "Symbol";
     this.name = name.toString();
