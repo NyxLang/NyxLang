008263084b6d1f99d02e260bcbb58d7b024fd3a4
Jason Barr <jason@jasonsbarr.com>
2021-04-06 12:34:21 -0500

    Stub out builtin type constructors

--- src/objects/builtins.js
+++ src/objects/builtins.js
@@ -6,3 +6,52 @@ const complex = require("./Numbers")["Complex"];
 const decimal = require("./Numbers")["Decimal"];
 const symbol = require("./Symbol")["Symbol"];
 const array = require("./Array")["Array"];
+const dict = require("./Dict")["Dict"];
+
+function Obj(obj) {
+  return obj;
+}
+
+function Double(num) {
+  return num;
+}
+
+function Decimal(num) {
+  return num;
+}
+
+function Fraction(num) {
+  return num;
+}
+
+function Complex(num) {
+  return num;
+}
+
+function Str(str) {
+  return str;
+}
+
+function Sym(sym) {
+  return sym;
+}
+
+function Arr(arr) {
+  return arr;
+}
+
+function Dict(d) {
+  return d;
+}
+
+module.exports = {
+  Object: Obj,
+  Double,
+  Decimal,
+  Fraction,
+  Complex,
+  String: Str,
+  Symbol: Sym,
+  Array: Arr,
+  Dict,
+};
