06184b8c74fdbb574eee08a517b42665db5cb813
Jason Barr <jason@jasonsbarr.com>
2021-04-15 12:29:12 -0500

    Move tests into test block

--- tests/numbers.test.js
+++ tests/numbers.test.js
@@ -16,18 +16,18 @@ describe("Testing basic numeric operations", () => {
     let output = Interpreter(code);
     expect(output.toString()).toBe(parseInt(output.toHexadecimal()).toString());
     expect(output.toNumber()).toBe(parseInt(output.toHexadecimal()));
+
+    code = "0b11001";
+    output = Interpreter(code);
+    // I have no idea why parseInt works with hex literals but not binary or octal
+    let outputStr = output.toBinary().split("b")[1];
+    expect(output.toString()).toBe(parseInt(outputStr, 2).toString());
+    expect(output.toNumber()).toBe(parseInt(outputStr, 2));
+
+    code = "0o755";
+    output = Interpreter(code);
+    outputStr = output.toOctal().split("o")[1];
+    expect(output.toString()).toBe(parseInt(outputStr, 8).toString());
+    expect(output.toNumber()).toBe(parseInt(outputStr, 8));
   });
-
-  code = "0b11001";
-  output = Interpreter(code);
-  // I have no idea why parseInt works with hex literals but not binary or octal
-  let outputStr = output.toBinary().split("b")[1];
-  expect(output.toString()).toBe(parseInt(outputStr, 2).toString());
-  expect(output.toNumber()).toBe(parseInt(outputStr, 2));
-
-  code = "0o755";
-  output = Interpreter(code);
-  outputStr = output.toOctal().split("o")[1];
-  expect(output.toString()).toBe(parseInt(outputStr, 8).toString());
-  expect(output.toNumber()).toBe(parseInt(outputStr, 8));
 });
